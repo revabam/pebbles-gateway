4bd3171ec6990184a910b9854ab2324b0c2c59f7
john.beineke <john.beineke@gmail.com>
2018-08-09 13:21:28 -0400

    Zuul Gateway-Service

---
+++ pebbles-gateway/src/main/java/com/revature/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,21 @@
+package com.revature.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@RestController
+@EnableZuulProxy
+@EnabaleEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+
+	}
+
+}
