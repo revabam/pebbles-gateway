e762b15a29ea9a7fe5213fbedfd2e927ff14af7e
BrandonScoggins44 <brandon.scoggins44@gmail.com>
2018-08-15 09:23:29 -0400

    updates for swagger

--- pebbles-gateway/src/main/java/com/revature/gatewayservice/GatewayServiceApplication.java
+++ pebbles-gateway/src/main/java/com/revature/gatewayservice/GatewayServiceApplication.java
@@ -2,14 +2,19 @@ package com.revature.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@RestController
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class GatewayServiceApplication {
 
@@ -18,4 +23,9 @@ public class GatewayServiceApplication {
 
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.revature"))
+				.paths(PathSelectors.any()).build();
+	}
 }
