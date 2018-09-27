b10595b0a039ae4a819f591f5cd818bbb685c9aa
Alicia <aliciadouglas26@gmail.com>
2018-09-27 10:11:12 -0400

    added fallback response

---
+++ src/main/java/com/revature/gatewayservice/config/HystrixFallbackConfiguration.java
@@ -0,0 +1,188 @@
+package com.revature.gatewayservice.config;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+@Configuration
+public class HystrixFallbackConfiguration {
+
+	@Bean
+	public FallbackProvider curriculumFallbackProvider() {
+		return new FallbackProvider() {
+
+			@Override
+			public String getRoute() {
+				return "pebbles-curriculum";
+			}
+
+
+			@Override
+			public ClientHttpResponse fallbackResponse(Throwable cause) {
+				return new ClientHttpResponse() {
+
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+					}
+
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream(
+								"{\"message\":\"Sorry, Curriculum Service is Down!\"}".getBytes());
+					}
+
+					@Override
+					public String getStatusText() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.toString();
+					}
+
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE;
+					}
+
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.value();
+					}
+
+					@Override
+					public void close() {
+					}
+				};
+			}
+
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+	
+	
+	@Bean
+	public FallbackProvider batchFallbackProvider() {
+		return new FallbackProvider() {
+
+			@Override
+			public String getRoute() {
+				return "pebbles-batch";
+			}
+
+
+			@Override
+			public ClientHttpResponse fallbackResponse(Throwable cause) {
+				return new ClientHttpResponse() {
+
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+					}
+
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream(
+								"{\"message\":\"Sorry, Batch Service is Down!\"}".getBytes());
+					}
+
+					@Override
+					public String getStatusText() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.toString();
+					}
+
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE;
+					}
+
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.value();
+					}
+
+					@Override
+					public void close() {
+					}
+				};
+			}
+
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+	
+	@Bean
+	public FallbackProvider calendarFallbackProvider() {
+		return new FallbackProvider() {
+
+			@Override
+			public String getRoute() {
+				return "pebbles-calendar";
+			}
+
+
+			@Override
+			public ClientHttpResponse fallbackResponse(Throwable cause) {
+				return new ClientHttpResponse() {
+
+					@Override
+					public HttpHeaders getHeaders() {
+						HttpHeaders headers = new HttpHeaders();
+						headers.setContentType(MediaType.APPLICATION_JSON);
+						return headers;
+					}
+
+					@Override
+					public InputStream getBody() throws IOException {
+						return new ByteArrayInputStream(
+								"{\"message\":\"Sorry, Calendar Service is Down!\"}".getBytes());
+					}
+
+					@Override
+					public String getStatusText() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.toString();
+					}
+
+					@Override
+					public HttpStatus getStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE;
+					}
+
+					@Override
+					public int getRawStatusCode() throws IOException {
+						return HttpStatus.SERVICE_UNAVAILABLE.value();
+					}
+
+					@Override
+					public void close() {
+					}
+				};
+			}
+
+			@Override
+			public ClientHttpResponse fallbackResponse() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
+
+}
